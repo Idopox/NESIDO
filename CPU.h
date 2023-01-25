10a99da1e4e555f3813374fce3f67c13a653ebcd
ido_barkan <idodobarkan@gmail.com>
2023-01-25 20:29:05 +0200

    bugfix, added clock function

--- CPU.h
+++ CPU.h
@@ -70,7 +70,6 @@ private:
 
 	struct INSTRUCTION
 	{
-		std::string name;		
 		uint8_t     (CPU::*operate )(void) = nullptr;
 		uint8_t     (CPU::*addrmode)(void) = nullptr;
 		uint8_t     cycles = 0;
@@ -83,11 +82,11 @@ private:
     uint8_t IMM();
     uint8_t IMP();
     uint8_t REL();
-    uint8_t ABS(eIndexReg reg = eIndexReg::None);
+    uint8_t ABS(eIndexReg reg);
     uint8_t AB0();
     uint8_t ABX();
     uint8_t ABY();
-    uint8_t ZPG(eIndexReg reg = eIndexReg::None);
+    uint8_t ZPG(eIndexReg reg);
     uint8_t ZP0();
     uint8_t IND();
     uint8_t ZPX();
