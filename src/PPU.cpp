05a13f17638f316c64968b47fa73cb100aa597f5
Idopox <idodobarkan@gmail.com>
2023-05-02 01:28:38 +0300

    basic cpu ppu read\write

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -22,6 +22,9 @@ uint8_t PPU::cpuRead(uint16_t addr)
 	case 0x0001: // Mask
 		break;
 	case 0x0002: // Status
+		data = (status.reg & 0xE0) | (ppuDataBuffer & 0x1F);
+		status.verticalBlank = 0;
+		address.addressLatch = false;
 		break;
 	case 0x0003: // OAM Address
 		break;
@@ -29,9 +32,15 @@ uint8_t PPU::cpuRead(uint16_t addr)
 		break;
 	case 0x0005: // Scroll
 		break;
-	case 0x0006: // PPU Address
+	case 0x0006: // PPU Address - not readable
 		break;
 	case 0x0007: // PPU Data
+		data = ppuDataBuffer;
+		ppuDataBuffer = ppuRead(address.fullAddr);
+
+		if (address.fullAddr > 0x3F00) data = ppuDataBuffer;
+
+		address.increment(control.bits.vram_addr_increment());
 		break;
 	}
 
@@ -43,8 +52,10 @@ void PPU::cpuWrite(uint16_t addr, uint8_t  data)
     switch (addr)
 	{
 	case 0x0000: // Control
+		control.reg = data;
 		break;
 	case 0x0001: // Mask
+		mask.reg = data;
 		break;
 	case 0x0002: // Status
 		break;
@@ -55,8 +66,12 @@ void PPU::cpuWrite(uint16_t addr, uint8_t  data)
 	case 0x0005: // Scroll
 		break;
 	case 0x0006: // PPU Address
+		address.update(data);
 		break;
 	case 0x0007: // PPU Data
+		ppuWrite(address.fullAddr,data);
+
+		address.increment(control.bits.vram_addr_increment());
 		break;
 	}
 }
@@ -66,8 +81,28 @@ uint8_t PPU::ppuRead(uint16_t addr)
     uint8_t data = 0x00;
 	addr &= 0x3FFF;
 
-    cart->ppuRead(data,addr);
 
+    if (cart->ppuRead(data,addr))
+	{
+		
+	}
+	else if (addr >= 0x0000 && addr <= 0x1FFF)
+	{
+		data = tblPattern[(addr & 0x1000) >> 12][addr & 0x0FFF];
+	}
+	else if (addr >= 0x2000 && addr <= 0x3EFF)
+	{
+		
+	}
+	else if (addr >= 0x3F00 && addr <= 0x3FFF)
+	{
+		addr &= 0x001F;
+		if (addr == 0x0010) addr = 0x0000;
+		if (addr == 0x0014) addr = 0x0004;
+		if (addr == 0x0018) addr = 0x0008;
+		if (addr == 0x001C) addr = 0x000C;
+		data = tblPalette[addr] & (mask.greyscale ? 0x30 : 0x3F);
+	}	
 
 	return data;
 }
@@ -75,7 +110,31 @@ uint8_t PPU::ppuRead(uint16_t addr)
 void PPU::ppuWrite(uint16_t addr, uint8_t data)
 {
     addr &= 0x3FFF;
-    cart->ppuWrite(data, addr);
+	
+	cart->ppuWrite(data, addr);
+
+	if (cart->ppuRead(data,addr))
+	{
+		
+	}
+	else if (addr >= 0x0000 && addr <= 0x1FFF)
+	{
+		tblPattern[(addr & 0x1000) >> 12][addr & 0x0FFF] = data;
+	}
+	else if (addr >= 0x2000 && addr <= 0x3EFF)
+	{
+		
+	}
+	else if (addr >= 0x3F00 && addr <= 0x3FFF)
+	{
+		addr &= 0x001F;
+		if (addr == 0x0010) addr = 0x0000;
+		if (addr == 0x0014) addr = 0x0004;
+		if (addr == 0x0018) addr = 0x0008;
+		if (addr == 0x001C) addr = 0x000C;
+		tblPalette[addr] = data;
+	}
+
 
 }
 
@@ -86,6 +145,24 @@ void PPU::ConnectCartridge(const std::shared_ptr<Cartridge>& cartridge)
 
 void PPU::clock()
 {
+
+	// top left of the screen means we finished the vblank phase
+	if (scanline == -1 && cycle == 1) 
+	{
+		status.verticalBlank = 1;
+	}
+	
+	//starting vblank phase
+	if (scanline == 241 && cycle == 1)
+	{
+		status.verticalBlank = 1;
+		if (control.bits.enableNMI)
+		{
+			nmi = true;
+		}
+		
+	}
+
     cycle++;
 	if (cycle >= 341)
 	{
--- src/PPU.h
+++ src/PPU.h
@@ -12,15 +12,124 @@ public:
     ~PPU();
 
 private:
-    uint8_t tblName[2][1024];
-    uint8_t tblPattern[2][4096];
-    uint8_t tblPalette[32];
+    uint8_t tblName[2][1024]; // VRAM
+    uint8_t tblPattern[2][4096]; // CHR ROM
+    uint8_t tblPalette[32]; // Palettes
 
 private:
     int16_t scanline = 0;
 	int16_t cycle = 0;
 
     bool frame_complete = false;
+
+private:
+	union 
+	{
+		struct 
+		{
+			uint8_t unused : 5;
+			uint8_t spriteOverflow : 1;
+			uint8_t sprite0hit : 1;
+			uint8_t verticalBlank :1;
+		};
+		uint8_t reg;
+		
+	}status;
+
+	union 
+	{
+		struct Bits
+		{
+			uint8_t tblNameX : 1;
+			uint8_t tblNameY : 1;
+			uint8_t vramIncrement : 1;
+			uint8_t patternSprite : 1;
+			uint8_t patternBackground : 1;
+			uint8_t spriteSize : 1;
+			uint8_t slaveMode : 1;
+			uint8_t enableNMI : 1;
+
+			uint8_t vram_addr_increment() const {
+        		return vramIncrement ? 32 : 1;
+    		}
+		}bits;
+		uint8_t reg;
+		
+	}control;
+
+	union 
+	{
+		struct 
+		{
+			uint8_t greyscale : 1;
+			uint8_t showBackgroundLeft : 1;
+			uint8_t showSpritesLeft : 1;
+			uint8_t showBackground : 1;
+			uint8_t showSprites : 1;
+			uint8_t red : 1;
+			uint8_t green : 1;
+			uint8_t blue : 1;
+
+		};
+		uint8_t reg;
+		
+	}mask;
+
+	union loopy_register
+	{
+		// Credit to Loopy for working this out :D
+		struct
+		{
+
+			uint16_t coarse_x : 5;
+			uint16_t coarse_y : 5;
+			uint16_t nametable_x : 1;
+			uint16_t nametable_y : 1;
+			uint16_t fine_y : 3;
+			uint16_t unused : 1;
+		};
+
+		uint16_t reg = 0x0000;
+	};
+	
+	
+	loopy_register vram_addr; // Active "pointer" address into nametable to extract background tile info
+	loopy_register tram_addr; // Temporary store of information to be "transferred" into "pointer" at various times
+
+    struct Address {
+        uint16_t fullAddr = 0x0000;
+        bool addressLatch = false;
+
+        uint8_t getLowAddr() const {
+            return (uint8_t)(fullAddr & 0xFF);
+        }
+
+        uint8_t getHiAddr() const {
+            return (uint8_t)((fullAddr >> 8) & 0xFF);
+        }
+
+        void update(uint8_t data) {
+            if (addressLatch) {
+				fullAddr = (fullAddr & 0xFF00) | (uint16_t)(data);
+            } else {
+			    fullAddr = (fullAddr & 0x00FF) | ((uint16_t)(data) << 8);
+            }
+            addressLatch = !addressLatch;
+        }
+
+		void increment(uint8_t inc) {
+			uint16_t prevAddr = fullAddr;
+			fullAddr += inc;
+
+			if (fullAddr > 0x3FFF) {
+				// Mirror down addr above 0x3FFF
+				fullAddr &= 0x3FFF;
+			}
+		}
+    } address;
+	
+	uint8_t ppuDataBuffer = 0x00;	
+	
 
 public:
 	// Communications with Main Bus
@@ -39,6 +148,8 @@ public:
 	// Interface
 	void ConnectCartridge(const std::shared_ptr<Cartridge>& cartridge);
 	void clock();
+
+	bool nmi = false;
 
 
 };
